eb14f467d5b7b0729ea093377683377ed274aee0
PandaMun <ansxogh97@gmail.com>
2024-10-01 18:49:57 +0900

    commit init

---
+++ src/test/java/com/taehomun/automation_server/AutomationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taehomun.automation_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
