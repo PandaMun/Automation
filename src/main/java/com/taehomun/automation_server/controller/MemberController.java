955ea67a858c81dcdd1cc0308e0f32aef39a688d
PandaMun <ansxogh97@gmail.com>
2024-10-04 01:07:00 +0900

    update member

--- src/main/java/com/taehomun/automation_server/controller/MemberController.java
+++ src/main/java/com/taehomun/automation_server/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.taehomun.automation_server.controller;
 
 
 import com.taehomun.automation_server.dto.MemberDto;
+import com.taehomun.automation_server.dto.ResponseDto;
 import com.taehomun.automation_server.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,24 +20,36 @@ public class MemberController {
 
 
     @PostMapping("/regist")
-    public ResponseEntity registerMember(@RequestBody MemberDto member) {
+    public ResponseDto registerMember(@RequestBody MemberDto member) {
 
         try{
             memberService.createMember(member);
         }catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            return ResponseDto.builder()
+                    .message("회원등록에 실패하였습니다.")
+                    .status("Fail")
+                    .build();
         }
-        return ResponseEntity.ok().build();
+        return ResponseDto.builder()
+                .status("Success")
+                .message("회원등록에 성공하였습니다.")
+                .build();
 
     }
 
     @PostMapping("/update")
-    public ResponseEntity updateMember(@RequestBody MemberDto member) {
+    public ResponseDto updateMember(@RequestBody MemberDto member) {
         try{
             memberService.updateMember(member);
         }catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            return ResponseDto.builder()
+                    .message("회원등록에 실패하였습니다.")
+                    .status("Fail")
+                    .build();
         }
-        return ResponseEntity.ok().build();
+        return ResponseDto.builder()
+                .status("Success")
+                .message("회원등록에 성공하였습니다.")
+                .build();
     }
 }
